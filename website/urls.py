b79d2be3809a2d6acd520a07155daf8dffcf159b
saitdemirci <151685161+saitdemirci@users.noreply.github.com>
2023-12-27 22:29:59 +0300

    Add files via upload

---
+++ website/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_view
+
+
+urlpatterns =[
+    path('',views.home,name="home"),
+    path('contact',views.contact,name="contact"),
+    path('about',views.about,name="about"),
+    # path('login',auth_view.LoginView.as_view(template_name='registration/login.html'),name="login"),
+]
