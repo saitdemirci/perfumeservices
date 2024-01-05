76a176ec7f0d120c1b5e68c9d1169541e09ed5f7
saitdemirci <151685161+saitdemirci@users.noreply.github.com>
2024-01-06 01:07:20 +0300

    change design

--- website/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Contact(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    subject = models.CharField(max_length=255)
-    message = models.TextField()
