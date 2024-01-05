4ffb85e07476f8f2f25de67bc08b769ee571acf1
saitdemirci <151685161+saitdemirci@users.noreply.github.com>
2024-01-06 01:30:33 +0300

    simple change

--- website/urls.py
+++ website/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_view
 urlpatterns =[
     path('',views.home,name="home"),
     path('contact',views.contact,name="contact"),
-    path('about',views.about,name="about"),
+    path('freequte',views.about,name="about"),
     path('contactform',views.contactform,name="contactform"),
     # path('login',auth_view.LoginView.as_view(template_name='registration/login.html'),name="login"),
 ]
